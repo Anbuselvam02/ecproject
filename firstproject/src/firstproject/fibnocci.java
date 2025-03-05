51d9057429cda484e96f5083ed86d98a9c3e8d36
Anbuselvam02 <Anbuselvam02>
2025-03-05 12:30:00 +0530

    fibbonoci

--- firstproject/src/firstproject/fibnocci.java
+++ firstproject/src/firstproject/fibnocci.java
@@ -1,5 +1,27 @@
 package firstproject;
 
+import java.util.Scanner;
+
 public class fibnocci {
 
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("n:");
+		
+		int n=sc.nextInt();
+		
+		int first=1, second=1;
+		
+		System.out.print(first +" "+second +" ");
+		
+		for (int i=3;i<=n;i++) {
+			int next=first+second;
+			System.out.print(next +" ");
+			
+			
+			second=first;
+			second=next;
+		}
+	}
 }
