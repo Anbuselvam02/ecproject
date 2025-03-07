9835dc33557a0feb1f1d895b86b087a28cd498c4
Anbuselvam02 <Anbuselvam02>
2025-03-07 16:12:34 +0530

    modified

--- myproject/src/myproject/Jdpcconnection.java
+++ myproject/src/myproject/Jdpcconnection.java
@@ -2,6 +2,7 @@ package myproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,10 +17,28 @@ public class Jdpcconnection {
 
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM student");
+			PreparedStatement pstmt = con.prepareStatement("INSERT INTO student (stu_name, age, address,mobile_num) VALUES (?, ?, ?,?)");
+//			pstmt.setString(1, "Alice");
+//			pstmt.setInt(2, 23);
+//			pstmt.setString(3, "Boston");
+//			pstmt.setLong(4, 994188451);
+//			pstmt.addBatch();
+//
+//			pstmt.setString(1, "Bob");
+//			pstmt.setInt(2, 24);
+//			pstmt.setString(3, "Chicago");
+//			pstmt.setLong(4, 994184951);
+//			
+//			pstmt.addBatch();
+//
+//			pstmt.executeBatch();  // Faster insertion
+
+			
+
 			
 
 			while(rs.next()) {
-				// Ensure correct data type retrieval
+				 //Ensure correct data type retrieval
 				System.out.println(rs.getInt(1)+"\t"+rs.getString(2) + "\t" + rs.getInt(3) + "\t" + rs.getString(4) + "\t"+"\t" + rs.getLong(5));
 
 			}
