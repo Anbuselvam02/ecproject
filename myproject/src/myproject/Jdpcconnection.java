d00ff453bbe475050576fb6e26ffff0116b63011
Anbuselvam02 <Anbuselvam02>
2025-03-07 16:14:16 +0530

    modified

--- myproject/src/myproject/Jdpcconnection.java
+++ myproject/src/myproject/Jdpcconnection.java
@@ -12,7 +12,7 @@ public class Jdpcconnection {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		try {
-			// Ensure correct database name
+		
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "");
 
 			Statement stmt = con.createStatement();
@@ -38,7 +38,6 @@ public class Jdpcconnection {
 			
 
 			while(rs.next()) {
-				 //Ensure correct data type retrieval
 				System.out.println(rs.getInt(1)+"\t"+rs.getString(2) + "\t" + rs.getInt(3) + "\t" + rs.getString(4) + "\t"+"\t" + rs.getLong(5));
 
 			}
