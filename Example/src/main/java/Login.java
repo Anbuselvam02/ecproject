f6ebc95710480e69efc852ac10a4c992a2478697
Anbuselvam02 <Anbuselvam02>
2025-03-10 17:27:07 +0530

    Jsp

---
+++ Example/src/main/java/Login.java
@@ -0,0 +1,60 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		User user=new User(username,password);
+		
+		 Logindb logindb=new Logindb();
+		 try {
+			if(logindb.insert(user)) {
+				response.sendRedirect("Loginsuss.jsp");
+				 
+			 }else {
+				 response.sendRedirect("NewFile.jsp");
+				 
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Example/src/main/webapp/NewFile.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Student login form</title>
+</head>
+<body>
+<div align="centre"> <h1>Student login Form</h1></div>
+<form action="login" method ="post">
+<div align="centre"> 
+<label> UserName:<input type="text" id="username" name ="username"></label>
+
+</div>
+<br>
+<div align="centre">
+<label> password:<input type="text" id="password" name ="username"></label>
+</div>
+<br>
+<div align="centre">
+<input type="submit" value ="login">
+</div>
+
+</form>
+
+</body>
+</html>
